db4ab84dfebff15a67bed7f5ba789c732ac4e153
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-07-29 10:34:25 -0400

    added some model material....but server.js won't load

---
+++ models/workout.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+      type: String,
+      trim: true,
+      required: "Username is Required"
+    },
+  
+    exercises: [
+      {
+        type: {
+          type: String,
+          enum: ['resistance', 'cardio'],
+          description: 'Cardio or Resistance Training Only'
+        },
+        name: {
+          type: String,
+          required: true,
+          trim: true
+        },
+        distance: Number,
+        duration: Number,
+        weight: Number,
+        sets: Number,
+        reps: Number
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
+  
+UserSchema.methods.coolifier = function() {
+this.username = `${this.username}...the Coolest!`;
+return this.username;
+};
+  
+
+  
+const Workout = mongoose.model("workout", WorkoutSchema);
+module.exports = Workout; 
+  
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan"); 
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./models/userModel");
-
 const app = express();
+
+app.use(logger("dev")); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false
+    }
+);
 
 app.use(require('./routes/api-routes.js'));
 app.use(require('./routes/html-routes.js')); 
