082b40cc485549e8c7b607bc22c62ddf7551418e
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-07-30 13:48:14 -0400

    fixed the routes, had a period that was throwing it all off

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router(); 
 const db = require('../models'); 
 
-router.post('./api/workouts/', ({ body }, res) => {
+router.post('/api/workouts', ({ body }, res) => {
     db.Workout.create(body)
     .then(dbWorkout => { res.json(dbWorkout) } )
     .catch(err => { res.json(err) });
 });
 
-router.put('./api/workouts/:id', (req, res) => {
+router.put('/api/workouts/:id', (req, res) => {
     db.Workout.findByIdAndUpdate(
         { _id: req.params.id }, 
         { $push: { exercises: req.body } }, 
@@ -38,7 +38,7 @@ router.put('./api/workouts/:id', (req, res) => {
 //     });
 // });
 
-router.get('./api/workouts', (req, res) => {
+router.get('/api/workouts', (req, res) => {
     db.Workout.aggregrate([
         {
             $addFields: {
@@ -53,10 +53,10 @@ router.get('./api/workouts', (req, res) => {
     .catch(err => { res.json(err); })
 });
 
-// router.delete('./api/workouts', ({ body }, res) => {
-//     db.Workout.findByIdAndDelete(body.id)
-//     .then(() => { res.json(true); })
-//     .catch(err => { res.json(err); });
-// });
+router.delete('/api/workouts', ({ body }, res) => {
+    db.Workout.findByIdAndDelete(body.id)
+    .then(() => { res.json(true); })
+    .catch(err => { res.json(err); });
+});
 
 module.exports = router;
