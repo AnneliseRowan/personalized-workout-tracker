bc651026cc422ad04dcc1c54de46455eb3e3f432
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-07-30 16:17:40 -0400

    got api-routes to work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,38 +17,29 @@ router.put('/api/workouts/:id', (req, res) => {
     .catch(err => { res.json(err); });
 });
 
-// router.get('./api/workouts/range', (req, res) => {
-//     db.Workout.find({})
-//     .then(dbWorkouts => {
-//         const workouts = dbWorkouts.map(workout => {
-//             console.log(workout);
-//             const duration = workout.exercises.reduce((acc,next) => {
-//                 return acc + next.duration;
-//             }, 0);
+router.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkouts => {
+        const workouts = dbWorkouts.map(workout => {
+            console.log(workout);
+            const duration = workout.exercises.reduce((acc,next) => {
+                return acc + next.duration;
+            }, 0);
 
-//             return {
-//                 totalDuration: duration, 
-//                 ...workout.toObject()
-//             }
-//         })
-//         res.json(workouts); 
-//     })
-//     .catch(err => {
-//         res.json(err); 
-//     });
-// });
+            return {
+                totalDuration: duration, 
+                ...workout.toObject()
+            }
+        })
+        res.json(workouts); 
+    })
+    .catch(err => {
+        res.json(err); 
+    });
+});
 
 router.get('/api/workouts', (req, res) => {
-    db.Workout.aggregrate([
-        {
-            $addFields: {
-                "totalDuration":
-                {
-                    $sum: "$exercises.duration"
-                },
-            },
-        },
-    ])
+    db.Workout.find({})
     .then(dbWorkout => { res.json(dbWorkout); })
     .catch(err => { res.json(err); })
 });
